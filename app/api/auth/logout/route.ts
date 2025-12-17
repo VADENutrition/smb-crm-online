3aa4acebc98b96eb5a4757ed1b7f3884677156ff
VADENutrition <131701345+VADENutrition@users.noreply.github.com>
2025-12-17 14:10:03 -0500

    Delete app directory

--- app/api/auth/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  const origin = req.nextUrl.origin;
-  const res = NextResponse.redirect(new URL("/", origin));
-  res.cookies.set("uid", "", { httpOnly: true, expires: new Date(0), sameSite: "lax", secure: true, path: "/" });
-  return res;
-}
